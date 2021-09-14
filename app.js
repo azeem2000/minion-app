803286705a665f61c3d4efbe738f411feb999d72
azeem2000 <61021388+azeem2000@users.noreply.github.com>
2021-09-14 13:09:08 +0530

    minion website javascript done

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 var btnTranslate = document.querySelector("#btn-translate")
 var textInput = document.querySelector("#text-input")
 var outputDiv = document.querySelector("#output")
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+
+function urlTranslate(text){
+    return serverURL+"?text="+text
+}
+
+function errorHandler(error){
+    console.log("Error Occured : "+ error)
+    alert("There seems to be a problem with server")
+}
 
 function clickHandler() {
-    outputDiv.innerText = "banananana " + textInput.value;
+    var inputText = textInput.value
+    fetch(urlTranslate(inputText))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated
+        outputDiv.innerText = translatedText
+    })
+    .catch(errorHandler)//This is a callback function
+    
 }
 
 btnTranslate.addEventListener("click",clickHandler)
